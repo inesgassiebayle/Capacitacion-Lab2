0d9e5c0eb079e1bbf33474d41fe53e9d0c3741fe
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-08-23 19:33:58 -0300

    delete-user fixed

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { tasksProviders } from './tasks.providers';
 import { TasksController } from './task.controller';
@@ -6,7 +6,7 @@ import { UsersModule } from '../users/users.module';
 import {TASK_REPOSITORY} from "../../core/constants";
 
 @Module({
-    imports: [UsersModule],
+    imports: [forwardRef(() => UsersModule)],
     controllers: [TasksController],
     providers: [TasksService, ...tasksProviders],
     exports: [TasksService, ...tasksProviders, TASK_REPOSITORY],
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { usersProviders } from './users.providers';
 import { UsersController } from './users.controller';
 import {TasksModule} from "../tasks/tasks.module";
 
 @Module({
-    imports: [TasksModule],
+    imports: [forwardRef(() => TasksModule)],
     controllers: [UsersController],
     providers: [UsersService, ...usersProviders],
     exports: [UsersService, ...usersProviders],
